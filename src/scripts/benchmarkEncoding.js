7105754c14ac400c769a8855743c2f3e7d6a5348
AXwain <5733365+AXwain@users.noreply.github.com>
2021-03-24 02:34:00 -0500

    Refactored the encoding benchmark script to use the new modules for getting the list of assets and the Encoders util

--- src/scripts/benchmarkEncoding.js
+++ src/scripts/benchmarkEncoding.js
@@ -1,27 +1,12 @@
 import { readFileSync, writeFileSync } from 'fs'
-import { opendir } from 'fs/promises'
-import { join } from 'path'
 
 import { max, mean, sampleVariance } from 'simple-statistics'
 import colors from 'colors/safe.js'
 
-import { Encode16 } from '../base16/encoder.js'
-import { Encode32 } from '../base32/encoder.js'
-import { Encode85 } from '../base85/encoder.js'
-import { Encode91 } from '../base91/encoder.js'
 import { buildHuffmanTree, encode, getHuffmanCodes, getHuffmanSymbolMap } from '../huffman/encoder.js'
+import { Encoders, EncodersKeys, Huffman } from '../utils/Encoders.js'
 import { formatByteSize } from '../utils/formatByteSize.js'
-
-function Encode64 (buffer) {
-  return buffer.toString('base64')
-}
-
-function EncodeFunc (encoder) {
-  return (buffer) => {
-    const Data = encoder(buffer)
-    return { data: Buffer.from(Data), size: Data.length }
-  }
-}
+import { getAssetList } from '../utils/getAssetList.js'
 
 function EncodeHuffman (buffer) {
   const Tree = buildHuffmanTree(buffer)
@@ -31,17 +16,6 @@ function EncodeHuffman (buffer) {
   return { data: Buffer.from(Data), size: Data.length + SymbolsSize }
 }
 
-const Huffman = 'Huffman'
-
-const Encoders = {
-  Base16: EncodeFunc(Encode16),
-  Base32: EncodeFunc(Encode32),
-  Base64: EncodeFunc(Encode64),
-  Base85: EncodeFunc(Encode85),
-  Base91: EncodeFunc(Encode91)
-}
-
-const EncodersKeys = Object.keys(Encoders)
 const Cases = []
 
 for (const Key of EncodersKeys) {
@@ -85,7 +59,6 @@ function ComputeBenchMarkResults (data) {
       Column.push(Row[i] * 100 / Row[FileSizeKey])
     }
     result += `${Cases[i]},${mean(Column).toFixed(3)},${sampleVariance(Column).toFixed(3)},${max(Column).toFixed(3)}\n`
-    Column.length = 0
   }
 
   return result
@@ -106,27 +79,14 @@ function DataToString (data) {
   return sizeTable + percentageTable
 }
 
-async function benchmark (path) {
-  const Regex = /\.place*/i
-  const Files = []
+async function benchmark (path, excludeRegex) {
   try {
     console.info(colors.black.bold('>Getting Asset File List'))
-    const Dir = await opendir(path)
-    for await (const Dirent of Dir) {
-      if (Dirent.isDirectory) {
-        const SubDir = await opendir(join(path, Dirent.name))
-        for await (const SubDirent of SubDir) {
-          if (SubDirent.isFile && !SubDirent.name.match(Regex)) {
-            Files.push(join(path, Dirent.name, SubDirent.name))
-          }
-        }
-      }
-    }
-
+    const Files = await getAssetList(path, excludeRegex)
     // Get File and Encoding Data
     const Data = []
     const StartTime = process.hrtime()
-    for (const File of Files) {
+    for await (const File of Files) {
       const Start = process.hrtime()
       process.stdout.write(`${colors.cyan.dim.bold('Testing file:')} ${File}`)
       Data.push(processFile(File))
@@ -147,4 +107,4 @@ async function benchmark (path) {
   }
 }
 
-benchmark('./assets').then(() => { console.log('Finished') })
+benchmark('./assets', /\.place*/i).then(() => { console.log('Finished') })
